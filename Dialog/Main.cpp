2e5de5a4be4f2ef9444fd93471bf0cc061cb2f9f
coulank <kr.smn6vbg@gmail.com>
2018-12-18 18:56:51 +0900

    String型中心に変更した
    経緯的にChar型から変更したという流れも反映させたほうが良いかなと思いました！
    こっちが完成版ですね！

--- Dialog/Main.cpp
+++ Dialog/Main.cpp
@@ -1,18 +1,35 @@
-#include "Main.h"
+#include <Windows.h>
+#include <stdlib.h>
+#include <string>
 
 int WinMain(
-	HINSTANCE hInstance,		// 現在のインスタンスハンドル
-	HINSTANCE hPrevInstance,	// 以前のインスタンスハンドル
-	LPSTR lpCmdLine,			// コマンドライン
-	int nCmdShow				// 表示状態
+    HINSTANCE hInstance,		// 現在のインスタンスハンドル
+    HINSTANCE hPrevInstance,	// 以前のインスタンスハンドル
+    LPSTR lpCmdLine,			// コマンドライン
+    int nCmdShow				// 表示状態
 ) {
-	char* str = lpCmdLine;
-	int count = StrCount(lpCmdLine);
-	if (str[0] != '\0') {
-		MessageBox(NULL, escape_str(++str), "確認", MB_OK);
-	}
-	else {
-		MessageBox(NULL, "コマンドライン引数から、"
-			"最初の一文字を除いてダイアログに表示されます", "使用方法", MB_OK);
-	}
+    char first = lpCmdLine[0];
+    if (first != '\0') {
+        std::string str = lpCmdLine;
+        std::string rep_str;
+        std::string::size_type pos = str.find("\\");
+        while (pos != std::string::npos) {
+            switch (str[pos + 1]) {
+            case 'n':
+                rep_str = "\n"; break;
+            case 't':
+                rep_str = "\t"; break;
+            case '\\':
+                rep_str = "\\"; break;
+            default:
+                rep_str = ""; break;
+            }
+            str.replace(pos, 2, rep_str);
+            pos = str.find("\\", rep_str.length());
+        }
+        MessageBox(NULL, str.c_str(), "確認", MB_OK);
+    }
+    else {
+        MessageBox(NULL, "コマンドライン引数からダイアログに表示されます", "使用方法", MB_OK);
+    }
 }
